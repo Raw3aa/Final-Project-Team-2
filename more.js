2646178bb197e0b1a012edaa941815ed35327799
Rawaa zaqqut <80100058+Raw3aa@users.noreply.github.com>
2022-06-27 12:01:59 +0300

    Add files via upload

---
+++ more.js
@@ -0,0 +1,51 @@
+import React from "react";
+import Paint from "./paint";
+function More() {
+  return (
+
+    <div>
+
+     <Paint
+       name = "Last Supper"
+        image= 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/The-Last-Supper-Restored-Da-Vinci_32x16.jpg/2560px-The-Last-Supper-Restored-Da-Vinci_32x16.jpg'
+        Description = 'This painting raised many questions about the personality of Leonardo da Vinci, as it contains many elements that do not belong to the traditional Christian concepts that the painting was originally painted to express. Some believe that this painting, among many of da Vinci s works, contains special references to a secret doctrine contrary to the all-powerful Catholic faith of the day,Many questions were raised about the character of John in Leonardo da Vinci s The Last Supper, in which his position was next to Christ, as the novel presented him as Mary Magdalene.'
+   />
+      <Paint
+         name = "jewish bride"
+         image = 'https://mybayutcdn.bayut.com/mybayut/wp-content/uploads/PaintingAR290620202-1024x640.jpg'
+         Description = 'The painting gained its current name in the early 19th century, when an Amsterdam art collector identified the subject as that of a Jewish father bestowing a necklace upon his daughter on her wedding day. This interpretation is no longer accepted, and the identity of the couple is uncertain.[2] The ambiguity is heightened by the lack of anecdotal context, leaving only the central universal theme, that of a couple joined in love.[3] Speculative suggestions as to the couple s identity have ranged from Rembrandt s son Titus and his bride, or Amsterdam poet Miguel de Barrios and his wife. Also considered are several couples from the Old Testament, including Abraham and Sarah, Boaz and Ruth, or Isaac and Rebekah, which is supported by a drawing by the artist several years prior.'
+ />
+      <Paint
+        name = "The First Step"
+        image = 'https://www.aljazeera.net/wp-content/uploads/2018/04/d262e3d6-0c1e-447b-9840-c100ade132e8.jpeg'
+        Description = '..'
+      />
+      <Paint
+        name =  "Honeymoon breakfast"
+        image =  'https://www.aljazeera.net/wp-content/uploads/2018/04/2f389db7-836b-4777-b845-6912b0014f7b.jpeg'
+        Description = "Daniel Ridgway Knight In his 'Honeymoon breakfast', we see a married couple working in the field on their honeymoon. The woman who spreads breakfast for her husband, in old clothes, from a wooden basket does not show any resentment about her work on the honeymoon. On the contrary, the look of the couple's eyes in the painting exudes love and intimacy. Everything, despite their poverty, is simple and reassuring."   
+     />
+     <Paint 
+        name =  "The First Dance"
+        image=  'https://www.aljazeera.net/wp-content/uploads/2018/04/699ac9ba-8217-451f-8bc5-0acdab495da9.jpeg'
+        Description = "In Magni's painting 'The First Dance'we see bright spirits who are not incapable of poverty. The grandfather who sat at the door of the house playing the accordion for his family smiles while the youngest member of the family dances his first dance with his older sister.The German painter Carl von Bergen '1853 - 1933', known for his great love of children, believed that a child's smile is the best that a painter can portray, and that the labors of mothers in making their children happy is the most important scene that a painting can truly present, We also see in the painting that the family lives in an old, simple, poor but happy country house. The whole family enjoys playing, even the mother claps for her young. Rich hearts in a simple house and on the throne of safety."
+      />
+      <Paint
+         name = "Chatting"
+        image = 'https://www.aljazeera.net/wp-content/uploads/2018/04/3359a35a-86c0-47d7-8057-2fb45ce4a584.jpeg'
+        Description = "Despite the dominance of the female element in de Blas's paintings, all of his paintings give the impression that happiness lies in ordinary life events with their simple details that are not weighed down by pretension. In the painting 'Chatting', 1905, we see four women chatting with a boy in a simple setting, wearing simple clothes, and also wearing bright smiles, As they work to earn their living, the atmosphere is not without joking and laughter."
+        />
+        <Paint
+        name = "Mona Lisa"
+        image = 'https://www.sayidaty.net/sites/default/files/styles/1375_scale/public/2020/02/11/6395046-777934270.jpg?itok=9PWG2uZ4'
+        Description = " visitors ) ,  However, the Mona Lisa did not gain this fame until the beginning of the twentieth century ; At that time, it was just one of the many paintings that were highly regarded.  With the beginning of the nineteenth century , people began to praise Da Vinci , and there was a belief at the time that he was a genius , and then the fame of the Mona Lisa began to grow gradually with the beginning of the mid-nineteenth century. The reason for her fame is due to several different reasons, the most important of which is her smilewhich has been described as vague and vague; It puzzled many people such as Sigmund Freud , Harvard professors , and countless viewers,  besides; His use of the method of drawing with a one-point perspective in the background, and there is something related to the bulk of the artistic composition of this painting , and the method he used in drawing aerial perspective, as each of them contributes to communicating the overall picture of this painting."
+         />
+        <Paint
+           name  = "Grandmother Is The Best"
+           image =  'https://www.aljazeera.net/wp-content/uploads/2018/04/776483b4-ee4f-4f5b-a38b-c2d553698b83.jpeg'
+           Description = "It is said that an Austrian painter named Adolf Hamburg (1847 - 1921) was asked to paint a realistic painting about a sense of help and safety, and he who specialized in drawing scenes of monks and monasticism, did not arrive and rushed to come up with a strange or abnormal idea,[2] he painted a very simple scene A grandmother sews her grandson's clothes while he wears them, lying on his stomach under her hands. The painting, titled Grandmother Is The Best, was a realistic depiction of a poor rural family living in a simple wooden house dressed in shabby clothes, but their simple house smelled of safety."
+         />
+         </div>
+  )
+}
+export default More;
